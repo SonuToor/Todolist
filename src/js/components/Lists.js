233dabb549f4acb56fc16c632e57016d215461c5
Sonu Toor <36528964+SonuToor@users.noreply.github.com>
2019-07-17 18:52:11 -0400

    renders todo list and correctly deletes items, not rendering done list correctly.

--- src/js/components/Lists.js
+++ src/js/components/Lists.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 export default class Lists extends React.Component {
+    state = { finishedItems : [] }
+    
+    handleDone = (event) => {
+        // need to access the actual text entered by the user here is what is missing 
+        console.log(event.target)
+        const listItem =  event.target.parentNode; 
+        console.log(listItem)
+        this.setState({ finishedItems : [listItem, ...this.state.finishedItems]});
+    }
+
+    handleDelete = (event) => {
+        const listItem =  event.target.parentNode
+        this.rootNode.removeChild(listItem)
+    }
 
     render() {
         return (
@@ -10,12 +24,18 @@ export default class Lists extends React.Component {
                     <h3>Done</h3>
                 </div>
                 <div className="lists">
-                    <ul className="todo-list list">
+                    <ul ref={node => this.rootNode = node}className="todo-list list">
+                        {this.props.tasks.map(task =>
+                            <li><button onClick={this.handleDone} className="button finished">&#10003;</button><button onClick={this.handleDelete} className="button delete">&#10007;</button>{task}</li>
+                        )}
                     </ul>
                     <ul className="done-list list">
+                        {/* {this.state.finishedItems.map(item => 
+                            <li className="finished-task">{item}</li>)} */}
                     </ul>
                 </div>
             </div>
         )
     }
 }
+
