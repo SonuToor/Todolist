9f935c219f88e4bc701a2f9319cbb8c4bcca972c
Sonu Toor <36528964+SonuToor@users.noreply.github.com>
2019-07-17 18:49:44 -0400

    Passes form data to parent component now

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 export default class Form extends React.Component {
-    handleSubmit(event) {
+    handleSubmit = (event) => {
         event.preventDefault();
         const task = event.target[0].value;
-        console.log(task);
+        this.props.onSubmit(task);
+        event.target[0].value = ""; 
     }
+    
     render() {
         return (
-            <form onSubmit={this.handleSubmit.bind(this)}  className="input">
-                <input className="input-task" placeholder="What do you need to get done?" minLength="5" required></input>
+            <form onSubmit={this.handleSubmit}  className="input">
+                <input className="input-task" placeholder="What do you need to get done?" minLength="3" required></input>
                 <button className="button submit">Add it to my list</button>
             </form>
         )
     }
 }
+
