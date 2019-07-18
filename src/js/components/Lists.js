4db79b2fd83ecf991ecd6862a97d663934cbbcf6
Sonu Toor <36528964+SonuToor@users.noreply.github.com>
2019-07-18 18:38:34 -0400

    Fully functional list now!

--- src/js/components/Lists.js
+++ src/js/components/Lists.js
@@ -4,16 +4,14 @@ export default class Lists extends React.Component {
     state = { finishedItems : [] }
     
     handleDone = (event) => {
-        // need to access the actual text entered by the user here is what is missing 
-        console.log(event.target)
-        const listItem =  event.target.parentNode; 
-        console.log(listItem)
-        this.setState({ finishedItems : [listItem, ...this.state.finishedItems]});
+        const listItem =  event.target.parentNode.lastChild; 
+        this.setState({finishedItems : [listItem, ...this.state.finishedItems]})
+        this.listNode.removeChild(event.target.parentNode)
     }
 
     handleDelete = (event) => {
         const listItem =  event.target.parentNode
-        this.rootNode.removeChild(listItem)
+        this.listNode.removeChild(listItem)
     }
 
     render() {
@@ -24,18 +22,17 @@ export default class Lists extends React.Component {
                     <h3>Done</h3>
                 </div>
                 <div className="lists">
-                    <ul ref={node => this.rootNode = node}className="todo-list list">
+                    <ul ref={list => this.listNode = list}className="todo-list list">
                         {this.props.tasks.map(task =>
                             <li><button onClick={this.handleDone} className="button finished">&#10003;</button><button onClick={this.handleDelete} className="button delete">&#10007;</button>{task}</li>
                         )}
                     </ul>
                     <ul className="done-list list">
-                        {/* {this.state.finishedItems.map(item => 
-                            <li className="finished-task">{item}</li>)} */}
+                        {this.state.finishedItems.map(task => 
+                            <li className="finished-task">{task.data}</li>)}
                     </ul>
                 </div>
             </div>
         )
     }
 }
-
